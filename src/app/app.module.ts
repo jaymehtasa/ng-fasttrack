f9068b2e3d76857b5d162a7236469d5b814d60bd
Jay Mehta <jay@solutionanalysts.com>
2020-01-08 16:05:15 +0530

    session #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { NewRecipeComponent } from './new-recipe/new-recipe.component';
+import { RecipeCellComponent } from './recipe-cell/recipe-cell.component';
+import { HighLightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewRecipeComponent,
+    RecipeCellComponent,
+    HighLightDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
