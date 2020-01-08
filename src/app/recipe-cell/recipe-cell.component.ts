0b34e18bf38a20fd17f1dc9be84b82ed0bbd5233
Jay Mehta <jay@solutionanalysts.com>
2020-01-08 19:59:21 +0530

    manually unsubscibe

--- src/app/recipe-cell/recipe-cell.component.ts
+++ src/app/recipe-cell/recipe-cell.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { RecipeManagerService } from '../recipe-manager.service';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-cell',
   templateUrl: './recipe-cell.component.html',
   styleUrls: ['./recipe-cell.component.scss']
 })
-export class RecipeCellComponent implements OnInit {
+export class RecipeCellComponent implements OnInit, OnDestroy {
   @Input() recipe;
+  private paramSubs: Subscription;
+
   constructor(
     private recipeManagerService: RecipeManagerService,
     private route: ActivatedRoute
@@ -17,8 +20,14 @@ export class RecipeCellComponent implements OnInit {
   ngOnInit() {
     const id = this.route.snapshot.params.id;
     this.recipe = this.recipeManagerService.getRecipe(id);
-    this.route.params.subscribe((params) => {
+    this.paramSubs = this.route.params.subscribe((params) => {
       this.recipe = this.recipeManagerService.getRecipe(params.id);
     });
+  }
+
+  ngOnDestroy() {
+    if (this.paramSubs) {
+      this.paramSubs.unsubscribe();
+    }
   }
 }
